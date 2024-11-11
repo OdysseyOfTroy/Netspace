9b727545f2208f020c7e8cf2197298614395b6bf
Troy Matthews <TroyMatthewsHong@gmail.com>
2024-11-11 16:59:28 +0000

    add a menu to choose what task to do

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-fn main() {
-    let mut  tasks: Vec<Task> = Vec::new();
-
-    tasks.push(Task::new("Learn rust basics".to_string()));
-
-    list_tasks(&tasks);
-}
+use std::io;
 struct Task {
     description: String,
     completed: bool,
@@ -14,14 +8,48 @@ impl Task {
     fn new(description: String) -> Task {
         Task {
             description,
-            completed: true,
+            completed: false,
+        }
+    }
+}
+fn main() {
+    let mut tasks: Vec<Task> = Vec::new();
+
+    loop {
+        println!("-- To-Do List Menu --");
+        println!("1. Add a new task");
+        println!("2. List all tasks");
+        println!("3. Exit");
+
+        let choice = get_user_input("Enter your option: ");
+
+        match choice.trim() {
+            "1" => {
+                let description = get_user_input("Enter a description: ");
+                tasks.push(Task::new(description));
+                println!("Task added!\n");
+            }
+            "2" => list_tasks(&tasks),
+            "3" => break,
+            _ => println!("Invalid choice, please try again. \n"),
         }
     }
 }
 
 fn list_tasks(tasks: &Vec<Task>) {
+    println!("\n");
     for (index, task) in tasks.iter().enumerate() {
         let status = if task.completed { "✓" } else { " " };
         println!("{}: [{}] {}", index + 1, status, task.description);
     }
+    println!("\n");
+}
+
+fn get_user_input(prompt: &str) -> String {
+    println!("{}", prompt);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+    input.trim().to_string()
 }
