4ef5892aebaa090e664230d70b296a038b161b16
Troy Matthews <TroyMatthewsHong@gmail.com>
2024-11-11 17:11:37 +0000

    added functionality to mark a task as complete

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ fn main() {
         println!("-- To-Do List Menu --");
         println!("1. Add a new task");
         println!("2. List all tasks");
-        println!("3. Exit");
+        println!("3. Mark a task as complete");
+        println!("4. Exit");
 
         let choice = get_user_input("Enter your option: ");
 
@@ -30,19 +31,33 @@ fn main() {
                 println!("Task added!\n");
             }
             "2" => list_tasks(&tasks),
-            "3" => break,
+            "3" => mark_task_as_complete(&mut tasks),
+            "4" => break,
             _ => println!("Invalid choice, please try again. \n"),
         }
     }
 }
 
 fn list_tasks(tasks: &Vec<Task>) {
-    println!("\n");
+    println!("\n-- Your Tasks --");
     for (index, task) in tasks.iter().enumerate() {
         let status = if task.completed { "✓" } else { " " };
         println!("{}: [{}] {}", index + 1, status, task.description);
     }
-    println!("\n");
+    println!("");
+}
+
+fn mark_task_as_complete(tasks: &mut Vec<Task>) {
+    list_tasks(&tasks);
+    let task_number = get_user_input("Enter the number of the task you wish to mark as completed");
+
+    match task_number.trim().parse::<usize>() {
+        Ok(num) if num > 0 && num <= tasks.len() => {
+            tasks[num - 1].completed = true;
+            println!("Task {} marked as complete!\n", num);
+        }
+        _ => println!("Invalid task number.\n"),
+    }
 }
 
 fn get_user_input(prompt: &str) -> String {
